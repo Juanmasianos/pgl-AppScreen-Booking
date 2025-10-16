07e75dcdc65cc5434ea182786b6caebad720692d
Juan Manuel <usr3507@salesianos-lacuesta.net>
2025-10-17 00:44:03 +0100

    feat(FooterItems.tsx): established the footer data

--- data/FooterItems.tsx
+++ data/FooterItems.tsx
@@ -1,5 +1,24 @@
 import { FooterItem } from "../types/FooterItem";
 
-//export const footerItems : FooterItem[] = {
-
-//}
+export const footerItems : FooterItem[] = [
+    {
+        key: '1',
+        icon: "search-outline",
+        title: "Search"
+    },
+    {
+        key: '2',
+        icon: "heart-outline",
+        title: "Saved"
+    },
+    {
+        key: '3',
+        icon: "briefcase-outline",
+        title: "Bookings"
+    },
+    {
+        key: '4',
+        picture: require("../assets/profile_picture.jpg"),
+        title: "Saved"
+    }
+]
